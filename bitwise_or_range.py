f88bb2fc587c9b5aa59c55bce41610eef1bc7f0f
Bill Cheng <chun-chan.cheng@viasat.com>
2019-03-22 12:28:44 -0500

    changed variable name

--- bitwise_or_range.py
+++ bitwise_or_range.py
@@ -3,17 +3,17 @@ from functools import reduce
 
 def bitwise_or_range(m, n):
     if len(bin(m)) != len(bin(n)):
-        i = max(m.bit_length(), n.bit_length())
-        return (1 << i) - 1
-    i = 0
+        left_shift = max(m.bit_length(), n.bit_length())
+        return (1 << left_shift) - 1
+    left_shift = 0
     while m != n:
         m >>= 1
         n >>= 1
-        i+=1
-    return (n << i) + ((1<<i) - 1)
+        left_shift += 1
+    return (n << left_shift) + ((1 << left_shift) - 1)
 
 def main():
-    a, b = 7, 100
+    a, b = 12, 18
     correct_res = reduce(lambda a, b: a | b, range(a, b+1))
     print(f'[{a}, {b}] bitwise or: {bitwise_or_range(a, b)}')
     # Testing Purpose
