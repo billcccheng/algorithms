ea4d53d00917949c6e610be45011f86b7e160857
Bill <iamkg05@gmail.com>
2019-03-23 14:43:31 -0500

    added random shuffle and better naming

--- quicksort.py
+++ quicksort.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-arr = [11, 10, 5, 7 ,8, 1, 3]
+import random
+arr = list(range(20))
+random.shuffle(arr)
 print('Original Array: ' + str(arr))
 
 # QUICKSORT
-def partition(a, low, high):
-    pivot = a[high]
-    i = low
+def partition(A, low, high):
+    pivot = low
     for j in range(low, high):
-        if a[j] <= pivot:
-            a[i], a[j] = a[j], a[i]
-            i+=1
-    a[i], a[high] = a[high], a[i]
-    return i
+        if A[j] <= A[high]:
+            A[pivot], A[j] = A[j], A[pivot]
+            pivot+=1
+    A[pivot], A[high] = A[high], A[pivot]
+    return pivot
 
 def quicksort(a, low, high):
     if low > high:
